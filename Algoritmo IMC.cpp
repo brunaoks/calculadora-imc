60eeec970434b0fbce318a78970ec6c2fb95c4ed
brunaoks <sfbruno2002@gmail.com>
2022-05-19 14:25:16 -0300

    implementação de biblioteca math.h e ajuste na impressão

--- Algoritmo IMC.cpp
+++ Algoritmo IMC.cpp
@@ -1,45 +1,48 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int main() {
+setlocale(LC_ALL,"Portuguese");
+	
 float massa;
 float altura;
 float IMC;
+cout << fixed;
+cout.precision(1);
 
-  cout << "Separe os decimais com ponto" << endl << endl;
-  cout << "Digite o valor do seu peso:";
+  cout << "OBS: Separe os decimais com ponto." << endl << endl;
+  cout << "Digite o valor do seu peso: ";
   cin >> massa;
-  cout << "Digite o valor da sua altura:";
+  cout << "Digite o valor da sua altura: ";
   cin >> altura;
 
-  IMC = massa/(altura*altura);
+  IMC = massa / pow(altura, 2);
 
   if (IMC <= 18.5) {
-  cout << "Abaixo do peso" << endl << endl;  
+  cout << "\nVocê está abaixo do peso ideal," << " e o valor do seu IMC é: " << IMC << endl;  
 }
    
    if (IMC >= 18.6 && IMC <= 24.9) {
-  cout << "Peso ideal" << endl << endl;  
+  cout << "\nVocê está com o peso ideal," << " e o valor do seu IMC é: " << IMC << endl;  
 }
  
     if (IMC >= 25.0 && IMC <= 29.9) {
-  cout << "Levemente acima do peso" << endl << endl;  
+  cout << "\nVocê está levemente acima do peso ideal," << " e o valor do seu IMC é: " << IMC << endl;  
 }
     if (IMC >= 30.0 && IMC <= 34.9) {
-  cout << "Obesidade de grau I" << endl << endl;  
+  cout << "\nVocê está com obesidade de grau I," << " e o valor do seu IMC é: " << IMC << endl;  
 }
  
   if (IMC >= 35.0 && IMC <= 39.9) {
-  cout << "Obesidade de grau II (Severa)" << endl << endl;  
+  cout << "\nVocê está com obesidade de grau II (Severa)," << " e o valor do seu IMC é: " << IMC << endl;  
 }
 
   if (IMC >= 40.0) {
-  cout << "Obesidade de grau III (Morbida)" << endl << endl;  
+  cout << "\nVocê está com obesidade de grau III (Morbida), " << " e o valor do seu IMC é: " << IMC << endl;  
 } 
-                 
-  cout << "O valor do seu IMC e:" << IMC << endl;
-
+	
 	system("pause");
 
   return 0;
